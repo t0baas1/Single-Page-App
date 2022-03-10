d57320c3a679a9bc5cb8d65ebf45e21cf3627913
unknown <topi.kanninen@sci.fi>
2022-03-10 22:15:29 +0200

    database-kikkailuja

---
+++ database/database/database/ApiContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using database.DbModels;
+
+namespace database
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Saved_Customers> Saved_Customers { get; set; }
+    }
+}
---
+++ database/database/database/DbModels/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace database.DbModels
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string  StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set;  }
+    }
+}
