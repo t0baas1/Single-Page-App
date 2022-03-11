54acd6123f9beb934109e2e29233e8f9def10e6d
kanninto <topi.a.a.kanninen@student.jyu.fi>
2022-03-11 17:07:12 +0200

    Toimiva request, hakee datan ja lisää tauluun

--- CustomerBackend/CustomerBackend/Program.cs
+++ CustomerBackend/CustomerBackend/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace CustomerBackend
 {
@@ -10,17 +13,41 @@ namespace CustomerBackend
     {
         static void Main(string[] args)
         {
-            var context = new CustomersEntities1();
-            var customer = new Customer()
+            string url = "http://www.filltext.com/?rows=100&pretty=true&id={index}&name={business}&address={addressObject}";
+            string vastaus = haedata(url);
+            //Console.WriteLine(vastaus);
+
+            parsija(vastaus);
+        }
+
+        public static string haedata(String url)
+        {
+            Console.WriteLine("Making API Call...");
+            using (var client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate }))
             {
-                CustomerId = 998,
-                Name = "Gugu gaga",
-                StreetAddress = "Grove St.",
-                City = "Constant Pain",
-                State = "GG",
-                Zip = "12345"
-            };
-            context.Customers.Add(customer);
+                //client.BaseAddress = new Uri("http://www.filltext.com/");
+                HttpResponseMessage response = client.GetAsync(url).Result;
+                response.EnsureSuccessStatusCode();
+                return response.Content.ReadAsStringAsync().Result;
+            }
+        }
+        public static void parsija(string vastaus)
+        {
+            var context = new CustomersEntities1();
+            dynamic dynJson = JsonConvert.DeserializeObject(vastaus);
+            foreach (var item in dynJson)
+            {
+                var customer = new Customer()
+                {
+                    CustomerId = item.id,
+                    Name = item.name,
+                    StreetAddress = item.address.streetAddress,
+                    City = item.address.city,
+                    State = item.address.state,
+                    Zip = item.address.zip,
+                };
+                context.Customers.Add(customer);
+            }
             context.SaveChanges();
         }
     }
