5c6bab8c02f9846671ccf69d1221efbc190ff655
kanninto <topi.a.a.kanninen@student.jyu.fi>
2022-03-11 16:53:11 +0200

    toimiva database

---
+++ CustomerBackend/CustomerBackend/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerBackend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new CustomersEntities1();
+            var customer = new Customer()
+            {
+                CustomerId = 998,
+                Name = "Gugu gaga",
+                StreetAddress = "Grove St.",
+                City = "Constant Pain",
+                State = "GG",
+                Zip = "12345"
+            };
+            context.Customers.Add(customer);
+            context.SaveChanges();
+        }
+    }
+}
