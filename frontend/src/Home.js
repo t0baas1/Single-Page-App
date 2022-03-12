71c3da7f8ed3c5e551e9758bd1580da57fac5138
kanninto <topi.a.a.kanninen@student.jyu.fi>
2022-03-12 21:49:53 +0200

    Fronttia alotettu

---
+++ frontend/src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Kaikki asiakkaat</h2>
+        <p>Tähän tulisi listaus kaikista tietokantaan tallenetuista asiakkaista.</p>
+      </div>
+    );
+  }
+}
+ 
+export default Home;
---
+++ frontend/src/Listaus.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+ 
+class Listaus extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Tallennetut asiakkaat</h2>
+        <ol>
+          <li>Asiakas 1</li>
+          <li>Asiakas 2</li>
+          <li>Asiakas 3</li>
+          <li>Asiakas 4</li>
+          <li>Asiakas 5</li>
+        </ol>
+      </div>
+    );
+  }
+}
+ 
+export default Listaus;
